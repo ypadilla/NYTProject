cd963c47ffa13e7b894237adda284aa0495b2499
Philip Van Schepen <pcvanschepen@gmail.com>
2016-08-02 20:51:18 -0400

    coded ajax pull

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-API key = 17f97c3e4f624814b0eb1d2cbc291270
+// API key = 17f97c3e4f624814b0eb1d2cbc291270
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "17f97c3e4f624814b0eb1d2cbc291270",
+  'q': "clinton"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
