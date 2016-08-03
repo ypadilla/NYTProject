37ac611615bcc41bd12177b1f2fe5fdd2a760680
Philip Van Schepen <pcvanschepen@gmail.com>
2016-08-02 21:30:55 -0400

    added onclick

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 // API key = 17f97c3e4f624814b0eb1d2cbc291270
+$("#searchButton").on("click", function () {
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	var term = "texas";
+	var numRecords = "";
+	var startYear = "";
+	var endYear = "";
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-var term = ""
-var numRecords = ""
-var startYear = ""
-var endYear = ""
-
-url += '?' + $.param({
-  'api-key': "17f97c3e4f624814b0eb1d2cbc291270",
-  'q': "clinton"
-});
-$.ajax({
-  url: url,
-  begin_date: startYear,
-  end_date: endYear,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+	url += '?' + $.param({
+	  'api-key': "17f97c3e4f624814b0eb1d2cbc291270",
+	  'q': term,
+	  // 'begin_date': startYear,
+	  // 'end_date': endYear,
+	});
+	$.ajax({
+	  url: url,
+	  method: 'GET',
+	}).done(function(result) {
+	  console.log(result);
+	}).fail(function(err) {
+	  throw err;
+	});
 });
