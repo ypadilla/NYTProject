01e1a4140d033a347a40ab7f773dc51dee499415
Philip Van Schepen <pcvanschepen@gmail.com>
2016-08-02 21:20:52 -0400

    added additional search params

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,19 @@
 // API key = 17f97c3e4f624814b0eb1d2cbc291270
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var term = ""
+var numRecords = ""
+var startYear = ""
+var endYear = ""
+
 url += '?' + $.param({
   'api-key': "17f97c3e4f624814b0eb1d2cbc291270",
   'q': "clinton"
 });
 $.ajax({
   url: url,
+  begin_date: startYear,
+  end_date: endYear,
   method: 'GET',
 }).done(function(result) {
   console.log(result);
